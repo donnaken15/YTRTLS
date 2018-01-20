fe44330dbc5c7652900efc8a4d42c2a7bdf10b30
donnaken15 <myminecraftfriend15@yahoo.com>
2018-01-19 19:00:23 -0500

    major update

--- YTRTLS/main.Designer.cs
+++ YTRTLS/main.Designer.cs
@@ -28,10 +28,19 @@
             this.thumbnail = new System.Windows.Forms.PictureBox();
             this.allstatsvideo = new System.Windows.Forms.Label();
             this.update = new System.Windows.Forms.Timer(this.components);
+            this.tooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.ytlogo2 = new System.Windows.Forms.PictureBox();
+            this.ytrtls2 = new System.Windows.Forms.Label();
+            this.url2 = new System.Windows.Forms.TextBox();
+            this.pfp = new System.Windows.Forms.PictureBox();
+            this.allstatschannel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ytlogo)).BeginInit();
             this.maincontrol.SuspendLayout();
+            this.channelpage.SuspendLayout();
             this.videopage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.thumbnail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ytlogo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pfp)).BeginInit();
             this.SuspendLayout();
             // 
             // ytlogo
@@ -61,15 +70,20 @@
             this.maincontrol.Location = new System.Drawing.Point(0, 0);
             this.maincontrol.Name = "maincontrol";
             this.maincontrol.SelectedIndex = 0;
-            this.maincontrol.Size = new System.Drawing.Size(251, 334);
+            this.maincontrol.Size = new System.Drawing.Size(230, 272);
             this.maincontrol.TabIndex = 2;
             // 
             // channelpage
             // 
+            this.channelpage.Controls.Add(this.url2);
+            this.channelpage.Controls.Add(this.pfp);
+            this.channelpage.Controls.Add(this.allstatschannel);
+            this.channelpage.Controls.Add(this.ytlogo2);
+            this.channelpage.Controls.Add(this.ytrtls2);
             this.channelpage.Location = new System.Drawing.Point(4, 22);
             this.channelpage.Name = "channelpage";
             this.channelpage.Padding = new System.Windows.Forms.Padding(3);
-            this.channelpage.Size = new System.Drawing.Size(243, 308);
+            this.channelpage.Size = new System.Drawing.Size(222, 246);
             this.channelpage.TabIndex = 1;
             this.channelpage.Text = "Channel";
             this.channelpage.UseVisualStyleBackColor = true;
@@ -84,7 +98,7 @@
             this.videopage.Location = new System.Drawing.Point(4, 22);
             this.videopage.Name = "videopage";
             this.videopage.Padding = new System.Windows.Forms.Padding(3);
-            this.videopage.Size = new System.Drawing.Size(243, 308);
+            this.videopage.Size = new System.Drawing.Size(222, 246);
             this.videopage.TabIndex = 0;
             this.videopage.Text = "Video";
             this.videopage.UseVisualStyleBackColor = true;
@@ -93,7 +107,7 @@
             // 
             this.url.Location = new System.Drawing.Point(4, 68);
             this.url.Name = "url";
-            this.url.Size = new System.Drawing.Size(123, 20);
+            this.url.Size = new System.Drawing.Size(108, 20);
             this.url.TabIndex = 4;
             this.url.TextChanged += new System.EventHandler(this.url_TextChanged);
             // 
@@ -101,7 +115,8 @@
             // 
             this.thumbnail.Location = new System.Drawing.Point(133, 6);
             this.thumbnail.Name = "thumbnail";
-            this.thumbnail.Size = new System.Drawing.Size(102, 50);
+            this.thumbnail.Size = new System.Drawing.Size(83, 50);
+            this.thumbnail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.thumbnail.TabIndex = 3;
             this.thumbnail.TabStop = false;
             // 
@@ -120,12 +135,66 @@
             this.update.Interval = 16;
             this.update.Tick += new System.EventHandler(this.update_Tick);
             // 
+            // tooltip
+            // 
+            this.tooltip.AutomaticDelay = 0;
+            this.tooltip.AutoPopDelay = 0;
+            this.tooltip.InitialDelay = 500;
+            this.tooltip.IsBalloon = true;
+            this.tooltip.ReshowDelay = 0;
+            this.tooltip.UseFading = false;
+            // 
+            // ytlogo2
+            // 
+            this.ytlogo2.Image = ((System.Drawing.Image)(resources.GetObject("ytlogo2.Image")));
+            this.ytlogo2.Location = new System.Drawing.Point(0, 0);
+            this.ytlogo2.Name = "ytlogo2";
+            this.ytlogo2.Size = new System.Drawing.Size(127, 53);
+            this.ytlogo2.TabIndex = 2;
+            this.ytlogo2.TabStop = false;
+            // 
+            // ytrtls2
+            // 
+            this.ytrtls2.AutoSize = true;
+            this.ytrtls2.Location = new System.Drawing.Point(7, 51);
+            this.ytrtls2.Name = "ytrtls2";
+            this.ytrtls2.Size = new System.Drawing.Size(105, 13);
+            this.ytrtls2.TabIndex = 3;
+            this.ytrtls2.Text = "Real Time Live Stats";
+            this.ytrtls2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // url2
+            // 
+            this.url2.Location = new System.Drawing.Point(6, 70);
+            this.url2.Name = "url2";
+            this.url2.Size = new System.Drawing.Size(108, 20);
+            this.url2.TabIndex = 7;
+            this.url2.TextChanged += new System.EventHandler(this.url2_TextChanged);
+            // 
+            // pfp
+            // 
+            this.pfp.Location = new System.Drawing.Point(135, 8);
+            this.pfp.Name = "pfp";
+            this.pfp.Size = new System.Drawing.Size(80, 80);
+            this.pfp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pfp.TabIndex = 6;
+            this.pfp.TabStop = false;
+            // 
+            // allstatschannel
+            // 
+            this.allstatschannel.AutoSize = true;
+            this.allstatschannel.Location = new System.Drawing.Point(5, 95);
+            this.allstatschannel.Name = "allstatschannel";
+            this.allstatschannel.Size = new System.Drawing.Size(65, 117);
+            this.allstatschannel.TabIndex = 5;
+            this.allstatschannel.Text = "Title:\r\n\r\nDescription:\r\n\r\nViews:\r\n\r\nSubscribers:\r\n\r\nVideos:";
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(251, 334);
+            this.ClientSize = new System.Drawing.Size(230, 272);
             this.Controls.Add(this.maincontrol);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "main";
@@ -134,9 +203,13 @@
             this.Text = "YTRTLS";
             ((System.ComponentModel.ISupportInitialize)(this.ytlogo)).EndInit();
             this.maincontrol.ResumeLayout(false);
+            this.channelpage.ResumeLayout(false);
+            this.channelpage.PerformLayout();
             this.videopage.ResumeLayout(false);
             this.videopage.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.thumbnail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ytlogo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pfp)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -152,6 +225,12 @@
         private System.Windows.Forms.Label allstatsvideo;
         private System.Windows.Forms.TextBox url;
         private System.Windows.Forms.Timer update;
+        private System.Windows.Forms.ToolTip tooltip;
+        private System.Windows.Forms.TextBox url2;
+        private System.Windows.Forms.PictureBox pfp;
+        private System.Windows.Forms.Label allstatschannel;
+        private System.Windows.Forms.PictureBox ytlogo2;
+        private System.Windows.Forms.Label ytrtls2;
     }
 }
 
--- YTRTLS/main.cs
+++ YTRTLS/main.cs
@@ -1,23 +1,131 @@
-﻿using System.Windows.Forms;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Windows.Forms;
 
 namespace YTRTLS
 {
     public partial class main : Form
     {
+        private static string vidinfo;
+
+        private static string userinfo;
+
+        private static WebClient metadata = new WebClient();
+
         public main()
         {
             InitializeComponent();
-            url.Text = System.Environment.GetCommandLineArgs()[1];
+            try
+            {
+                if (System.Environment.GetCommandLineArgs()[1] != "")
+                {
+                    url.Text = System.Environment.GetCommandLineArgs()[1];
+                }
+            }
+            catch { }
         }
         
         private void update_Tick(object sender, System.EventArgs e)
         {
+            try
+            {
+                thumbnail.Load("http://i.ytimg.com/vi/" + url.Text + "/default.jpg");
+            }
+            catch { }
+            if (url.Text != "")
+            {
+                try
+                {
+                    vidinfo = metadata.DownloadString("https://www.googleapis.com/youtube/v3/videos?id=" + url.Text + "&part=snippet,contentDetails,statistics&key=AIzaSyB8huSbFxBlMdpbKCC1DjeQPPOt96obnfY");
+                    allstatsvideo.Text = @"Title: " + SubstringExtensions.Between(vidinfo, "\"title\": \"", "\",\n    \"description\": \"") + @"
+
+Description: " + SubstringExtensions.Between(vidinfo, "\"description\": \"", "\",\n    \"thumbnails\": {") + @"
+
+Views: " + SubstringExtensions.Between(vidinfo, "\"viewCount\": \"", "\",\n    \"likeCount\": \"") + @"
+
+Likes: " + SubstringExtensions.Between(vidinfo, "\"likeCount\": \"", "\",\n    \"dislikeCount\": \"") + @"
+
+Dislikes: " + SubstringExtensions.Between(vidinfo, "\"dislikeCount\": \"", "\",\n    \"favoriteCount\": \"") + @"
+
+Comment count:  " + SubstringExtensions.Between(vidinfo, "\"commentCount\": \"", "\"\n   }\n  }\n ]") + @"";
+                }
+                catch { }
+            }
+            try
+            {
+                userinfo = metadata.DownloadString("https://www.googleapis.com/youtube/v3/channels?id=" + url2.Text + "&part=snippet,contentDetails,statistics&key=AIzaSyB8huSbFxBlMdpbKCC1DjeQPPOt96obnfY").Replace("\\n", Environment.NewLine);
+                string subscribers_check_hide = "0";
+                if (userinfo.Contains("\"hiddenSubscriberCount\": false,")) { subscribers_check_hide = SubstringExtensions.Between(userinfo, "\"subscriberCount\": \"", "\",\n    \"hiddenSubscriberCount\": \""); } else { subscribers_check_hide = "[REDACTED]"; }
+                pfp.Load(SubstringExtensions.Between(userinfo, "    \"thumbnails\": {\n     \"default\": {\n      \"url\": \"", "\"\n     },"));
+                allstatschannel.Text = @"Title: " + SubstringExtensions.Between(userinfo, "\"title\": \"", "\",\n    \"description\": \"") + @"
+
+Description: " + SubstringExtensions.Between(userinfo, "\"description\": \"", "\",\n    \"customUrl\": \"") + @"
+
+Views: " + SubstringExtensions.Between(userinfo, "\"viewCount\": \"", "\",\n    \"commentCount\": \"") + @"
+
+Subscribers: " + subscribers_check_hide + @"
+
+Videos: " + SubstringExtensions.Between(userinfo, "\"videoCount\": \"", "\"\n   }\n  }\n ]");
+            }
+            catch { }
 
         }
 
         private void url_TextChanged(object sender, System.EventArgs e)
         {
-            url.Text = url.Text.Replace("https", "").Replace("http", "").Replace("://", "").Replace("www.", "").Replace("youtube.com/", "").Replace("watch?v=", "").Replace("v/", "").Replace("youtu.be/", "").Replace(SubstringExtensions.After(url.Text, "&"), "").Replace("&", "");
+            try
+            {
+                url.Text = url.Text.Replace("https", "").Replace("http", "").Replace("://", "").Replace("www.", "").Replace("youtube.com/", "").Replace("watch?v=", "").Replace("v/", "").Replace("youtu.be/", "");
+                url.Text = url.Text.Replace(SubstringExtensions.After(url.Text, "&"), "").Replace("&", "");
+            }
+            catch { }
+            try
+            {
+                vidinfo = metadata.DownloadString("https://www.googleapis.com/youtube/v3/videos?id=" + url.Text + "&part=snippet,contentDetails,statistics&key=AIzaSyB8huSbFxBlMdpbKCC1DjeQPPOt96obnfY").Replace("\\n", Environment.NewLine);
+                allstatsvideo.Text = @"Title: " + SubstringExtensions.Between(vidinfo, "\"title\": \"", "\",\n    \"description\": \"") + @"
+
+Description: " + SubstringExtensions.Between(vidinfo, "\"description\": \"", "\",\n    \"thumbnails\": {") + @"
+
+Views: " + SubstringExtensions.Between(vidinfo, "\"viewCount\": \"", "\",\n    \"likeCount\": \"") + @"
+
+Likes: " + SubstringExtensions.Between(vidinfo, "\"likeCount\": \"", "\",\n    \"dislikeCount\": \"") + @"
+
+Dislikes: " + SubstringExtensions.Between(vidinfo, "\"dislikeCount\": \"", "\",\n    \"favoriteCount\": \"") + @"
+
+Comment count:  " + SubstringExtensions.Between(vidinfo, "\"commentCount\": \"", "\"\n   }\n  }\n ]") + @"";
+            }
+            catch { }
+            tooltip.SetToolTip(allstatsvideo, allstatsvideo.Text);
+        }
+
+        private void url2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                url2.Text = url2.Text.Replace("https", "").Replace("http", "").Replace("://", "").Replace("www.", "").Replace("youtube.com/", "").Replace("channel/", "").Replace("c/", "").Replace("user/", "").Replace("u/","");
+                url2.Text = url2.Text.Replace(SubstringExtensions.After(url.Text, "&"), "").Replace("&", "");
+            }
+            catch { }
+            try
+            {
+                userinfo = metadata.DownloadString("https://www.googleapis.com/youtube/v3/channels?id=" + url2.Text + "&part=snippet,contentDetails,statistics&key=AIzaSyB8huSbFxBlMdpbKCC1DjeQPPOt96obnfY").Replace("\\n", " ");
+                string subscribers_check_hide = "0";
+                if (userinfo.Contains("\"hiddenSubscriberCount\": false,")) { subscribers_check_hide = SubstringExtensions.Between(userinfo, "\"subscriberCount\": \"", "\",\n    \"hiddenSubscriberCount\": "); } else { subscribers_check_hide = "[REDACTED]"; }
+                //pfp.Load(SubstringExtensions.Between(userinfo, "\"url\": \"", "\"\n     },"));
+                allstatschannel.Text = @"Title: " + SubstringExtensions.Between(userinfo, "\"title\": \"", "\",\n    \"description\": \"") + @"
+
+Description: " + SubstringExtensions.Between(userinfo, "\"description\": \"", "\",\n    \"customUrl\": \"") + @"
+
+Views: " + SubstringExtensions.Between(userinfo, "\"viewCount\": \"", "\",\n    \"commentCount\": \"") + @"
+
+Subscribers: " + subscribers_check_hide + @"
+
+Videos: " + SubstringExtensions.Between(userinfo, "\"videoCount\": \"", "\"\n   }\n  }\n ]");
+            }
+            catch { }
+            tooltip.SetToolTip(allstatschannel, allstatschannel.Text);
         }
     }
 }
